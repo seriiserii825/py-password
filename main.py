86197624e8223416b49970d9403a31f9ed169caf
serii burduja <seriiburduja@gmail.com>
2024-10-16 08:23:49 +0300

    feat: align center

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+FONT_NAME = "Courier"
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -19,6 +21,31 @@ width, height = logo_img.width(), logo_img.height()
 
 canvas = Canvas(window, height=height, width=width, bg='white', highlightthickness=0)
 canvas.create_image(0, 0, image=logo_img, anchor=NW)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+label_website = Label(text="Timer", bg="white", font=(FONT_NAME, 12, "bold"))
+label_website.grid(row=1, column=0)
+
+input_website = Entry(window)
+input_website.grid(row=1, column=1, columnspan=2)
+
+label_email = Label(text="Email/Username", bg="white", font=(FONT_NAME, 12, "bold"))
+label_email.grid(row=2, column=0)
+
+input_email = Entry(window)
+input_email.grid(row=2, column=1, columnspan=2)
+
+label_password = Label(text="Password", bg="white", font=(FONT_NAME, 12, "bold"))
+label_password.grid(row=3, column=0)
+
+input_password = Entry(window)
+input_password.grid(row=3, column=1)
+
+button_generate_password = Button(text="Generate password", highlightthickness=0)
+button_generate_password.grid(row=3, column=2)
+
+
+button_add = Button(text="Add", highlightthickness=0)
+button_add.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
